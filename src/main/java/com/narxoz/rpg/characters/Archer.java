7696f5c38f844bada6e8f597bc5b5ada60464217
NurdauletZhussip-tech <nurdaulet.zhussip.studying@gmail.com>
2026-02-13 20:26:23 +0500

    Archer by assignments

--- src/main/java/com/narxoz/rpg/characters/Archer.java
+++ src/main/java/com/narxoz/rpg/characters/Archer.java
@@ -1,13 +1,8 @@
 package com.narxoz.rpg.characters;
 
-public class Archer implements GameCharacter {
+public class Archer extends GameCharacter {
     private String name;
-    public Archer(String name) { this.name = name; }
-    @Override
-    public void getStats() {
-        System.out.println(name + " [Archer] - HP: 100, Mana: 60, Str: 10, Int: 10");
-    }
-    @Override
+    public Archer(String name) { super(name, 150, 20, 15, 5);}
     public void useSpecialAbility() {
         System.out.println("Special Ability: Volley - Rains arrows on targets!");
     }
