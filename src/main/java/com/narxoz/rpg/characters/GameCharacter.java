3d6a2723feb900fcb14fe6548671cf6b0e7189fc
NurdauletZhussip-tech <nurdaulet.zhussip.studying@gmail.com>
2026-02-13 20:22:58 +0500

    Переделал чуть чуть под требования

--- src/main/java/com/narxoz/rpg/characters/GameCharacter.java
+++ src/main/java/com/narxoz/rpg/characters/GameCharacter.java
@@ -1,6 +1,23 @@
 package com.narxoz.rpg.characters;
 
-public interface GameCharacter {
-    void getStats();
-    void useSpecialAbility();
+public abstract class GameCharacter {
+    private String name;
+    private int health;
+    private int mana;
+    private int strength;
+    private int intelligence;
+
+    public GameCharacter(String name, int health, int mana, int strength, int intelligence) {
+        this.name = name;
+        this.health = health;
+        this.mana = mana;
+        this.strength = strength;
+        this.intelligence = intelligence;
+
+    }public abstract void useSpecialAbility();
+
+        public void getStats() {
+            System.out.println("Name: " + name + " | HP: " + health + " | Mana: " + mana +
+                    " | Str: " + strength + " | Int: " + intelligence);
+        }
 }
